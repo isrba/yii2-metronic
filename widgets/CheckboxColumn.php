e07e0321007e57b0e6e58f17863b7719b6cb782b
Ivan Srba <ivan.srba@stuba.sk>
2017-08-06 13:35:34 +0200

    Add new CheckboxColumn widget

---
+++ widgets/CheckboxColumn.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 Digital Deals s.r.o.
+ * @license http://www.digitaldeals.cz/license/
+ */
+
+namespace isrba\metronic\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+class CheckboxColumn extends \kartik\grid\CheckboxColumn {
+
+    /**
+     * @inheritdoc
+     */
+    protected function renderHeaderCellContent()
+    {
+        if ($this->header !== null || !$this->multiple) {
+            return parent::renderHeaderCellContent();
+        } else {
+
+            $content = Html::checkbox($this->getHeaderCheckBoxName(), false, ['class' => 'select-on-check-all']) .'<span></span>';
+            $content = Html::tag('label', $content, ['class' => 'mt-checkbox mt-checkbox-single mt-checkbox-outline']);
+
+            return $content;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        if ($this->checkboxOptions instanceof Closure) {
+            $options = call_user_func($this->checkboxOptions, $model, $key, $index, $this);
+        } else {
+            $options = $this->checkboxOptions;
+        }
+
+        if (!isset($options['value'])) {
+            $options['value'] = is_array($key) ? Json::encode($key) : $key;
+        }
+
+        if ($this->cssClass !== null) {
+            Html::addCssClass($options, $this->cssClass);
+        }
+
+        $content = Html::checkbox($this->name, !empty($options['checked']), $options) .'<span></span>';
+        $content = Html::tag('label', $content, ['class' => 'mt-checkbox mt-checkbox-single mt-checkbox-outline']);
+
+        return $content;
+    }
+}
