9b092b436a161ac45a8affd4b18cd6e94288c1d6
Ivan Srba <ivan.srba@stuba.sk>
2018-02-18 14:14:26 +0100

    Remove action label + redesign ActionColumn

--- widgets/ActionColumn.php
+++ widgets/ActionColumn.php
@@ -19,37 +19,37 @@ class ActionColumn extends \yii\grid\ActionColumn {
     /**
      * @var array the HTML options for the data cell tags.
      */
-    public $contentOptions = ['class' => 'text-center text-nowrap'];
+    public $contentOptions = ['class' => 'text-center text-nowrap actions'];
 
     /**
      * @var string the template that is used to render the content in each data cell.
      */
-    public $template = '{view}{update}';
+    public $template = '{view} {update}';
 
     /**
      * @var string the icon for the view button.
      */
-    public $viewButtonIcon = 'fa fa-search';
+    public $viewButtonIcon = 'icon-magnifier';
 
     /**
      * @var string the icon for the update button.
      */
-    public $updateButtonIcon = 'fa fa-edit';
+    public $updateButtonIcon = 'icon-note';
 
     /**
      * @var string the icon for the delete button.
      */
-    public $deleteButtonIcon = 'fa fa-trash';
+    public $deleteButtonIcon = 'icon-trash';
 
     /**
      * @var string btn view class
      */
-    public $btnViewClass = 'btn btn-outline btn-xs blue-madison';
+    public $btnViewClass = 'action-view';
 
     /**
      * @var string btn update class
      */
-    public $btnUpdateClass = 'btn btn-outline btn-xs blue-madison';
+    public $btnUpdateClass = 'action-update';
 
     /**
      * @var string btn delete class
@@ -64,32 +64,35 @@ class ActionColumn extends \yii\grid\ActionColumn {
         if (!isset($this->buttons['view']))
         {
             $this->buttons['view'] = function ($url, $model, $key) {
-                return Html::a('<i class="'.$this->viewButtonIcon.'"></i> View', $url, [
-                        'title' => \Yii::t('yii', 'View'),
-                        'data-pjax' => '0',
-                        'class' => $this->btnViewClass,
+                return Html::a('<i class="'.$this->viewButtonIcon.'"></i>', $url, [
+                    'class' => $this->btnViewClass,
+                    'data-toggle' => 'tooltip',
+                    'title' => \Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
                 ]);
             };
         }
         if (!isset($this->buttons['update']))
         {
             $this->buttons['update'] = function ($url, $model, $key) {
-                return Html::a('<i class="'.$this->updateButtonIcon.'"></i> Edit', $url, [
-                        'title' => \Yii::t('yii', 'Update'),
-                        'data-pjax' => '0',
-                        'class' => $this->btnUpdateClass,
+                return Html::a('<i class="'.$this->updateButtonIcon.'"></i>', $url, [
+                    'class' => $this->btnUpdateClass,
+                    'data-toggle' => 'tooltip',
+                    'title' => \Yii::t('yii', 'Update'),
+                    'data-pjax' => '0',
                 ]);
             };
         }
         if (!isset($this->buttons['delete']))
         {
             $this->buttons['delete'] = function ($url, $model, $key) {
-                return Html::a('<span class="'.$this->deleteButtonIcon.'"></span>', $url, [
-                        'title' => \Yii::t('yii', 'Delete'),
-                        'data-confirm' => \Yii::t('yii', 'Are you sure you want to delete this item?'),
-                        'data-method' => 'post',
-                        'data-pjax' => '0',
-                        'class' => $this->btnDeleteClass,
+                return Html::a('<i class="'.$this->deleteButtonIcon.'"></i>', $url, [
+                    'class' => $this->btnDeleteClass,
+                    'data-confirm' => \Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-toggle' => 'tooltip',
+                    'title' => \Yii::t('yii', 'Delete'),
+                    'data-pjax' => '0',
                 ]);
             };
         }
